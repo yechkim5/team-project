b7e3a6d0e60d605059c19e36350125fbb7e9a6dd
SanaTK1 <sana.hasheminia@gmail.com>
2025-11-10 11:15:18 -0500

    Outlined goals for week eleven lab

---
+++ src/main/java/entity/Battle.java
@@ -0,0 +1,7 @@
+/* The Battle class is what ties two teams of Pokemon together and will be the
+canvas that unites all other classes within our project. By week eleven, the
+Battle class will have the following functionalities: calculate turn order,
+keep track of modifiers on the stats and health of Pokemon in play, keep track
+of the two active Pokemon in play and shepard the effects of moves from one Pokemon
+to another.
+ */
