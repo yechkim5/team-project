6ebb5d23f4c349077bbc2775e16665ad5d2caa53
Sendhilishaan <ishaansendhil@gmail.com>
2025-11-10 18:06:46 -0500

    feat: get pokemon name, moves, type, and sprite

--- src/main/java/poke_api/pokemonFetcher.java
+++ src/main/java/poke_api/pokemonFetcher.java
@@ -7,12 +7,22 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 
 public class pokemonFetcher {
+
+    public static void main(String[] args) throws IOException {
+        pokemonFetcher pf = new pokemonFetcher();
+        JSONObject oj = pf.getPokemon("charmander");
+        String name = pf.getPokemonName(oj);
+        //System.out.println(name);
+
+        //System.out.println(pf.getPokemonMoves(oj));
+        System.out.println(pf.getPokemonType(oj));
+        System.out.println(Arrays.toString(pf.getPokemonSprite(oj)));
+    }
+
     private final OkHttpClient client = new OkHttpClient();
 
     private JSONObject getPokemon(String id) throws IOException {
@@ -20,7 +30,7 @@ public class pokemonFetcher {
         // other classes will use the JSON object to get more information
 
         Request request = new Request.Builder()
-                .url("https://pokeapi.co/api/v2/ability/" + id)
+                .url("https://pokeapi.co/api/v2/pokemon/" + id)
                 .build();
         try (Response response = client.newCall(request).execute()) {
 
@@ -31,10 +41,6 @@ public class pokemonFetcher {
             String body = response.body().string();
             JSONObject json = new JSONObject(body);
 
-            if (!json.getString("status").equals("success")) {
-                throw new IOException("Unexpected status " + json.getString("status"));
-            }
-
             return json;
 
 
@@ -43,17 +49,67 @@ public class pokemonFetcher {
         }
     }
 
-    private String getPokemonName(JSONObject pokemon) {
+    public String getPokemonName(JSONObject pokemon) {
         return pokemon.getString("name");
     }
 
-    private HashMap<String, String> getPokemonMoves(JSONObject pokemon) {
+    public HashMap<String, HashMap<String, Object>> getPokemonMoves(JSONObject pokemon) throws IOException {
         JSONArray moves = pokemon.getJSONArray("moves");
-        HashMap<String, String> move = new HashMap<>(4, 4);
+        HashMap<String, HashMap<String, Object>> move = new HashMap<>(4);
+
         for (int i = 0; i < moves.length(); i++) {
-            String url = moves.getJSONObject(i).getString("url");
+
+            JSONObject move_entry = moves.getJSONObject(i);
+            JSONObject move_object = move_entry.getJSONObject("move");
+            String url = move_object.getString("url");
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            try  (Response response = client.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                String body = response.body().string();
+                JSONObject json = new JSONObject(body);
+
+                HashMap<String, Object> move_info = new HashMap<>();
+                int pp = json.getInt("pp");
+                String name = json.getString("name");
+
+                // nul for status moves
+                Integer power = json.isNull("power") ? null : json.getInt("power");
+
+                JSONObject typeObject = json.getJSONObject("type");
+                String type = typeObject.getString("name");
+
+                move_info.put("type", type);
+                move_info.put("power", power);
+                move_info.put("pp", pp);
+
+                move.put(name, move_info);
+
+            }
+
         }
-        return 1;
+        return move;
+    }
+
+    public String getPokemonType(JSONObject pokemon) {
+        JSONArray t = pokemon.getJSONArray("types");
+        JSONObject type = t.getJSONObject(0);
+        JSONObject typeObject = type.getJSONObject("type");
+        return typeObject.getString("name");
+    }
+
+    public String[] getPokemonSprite(JSONObject pokemon) {
+        // returns [front, back] pngs for pokemon sprite
+        JSONObject spriteObj = pokemon.getJSONObject("sprites");
+        String front = spriteObj.getString("front_default");
+        String back = spriteObj.getString("back_default");
+        return new String[] {front, back};
     }
 }
 
