01308920a61dc151445dc3ece970da85317aad43
SanaTK1 <sana.hasheminia@gmail.com>
2025-11-10 11:04:36 -0500

    Added documentation for week eleven goals

--- src/main/java/entity/Pokemon.java
+++ src/main/java/entity/Pokemon.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/* By the lab of week eleven, the Pokemon class should be entirely complete so
+that users can possess a Pokemon with all the properties outlined in the
+project blueprint. This Pokemon class should emulate its functionality within
+the Pokemon games and have the ability to interact with other Pokemon using Moves.
+The effect of moves that cause ailments or change the stats of Pokemon will
+be tracked through the battle class as opposed to the Pokemon instance individually.
+ */
+
 public class Pokemon {
     /** Entity representing Pokemon as seen in the Pokemon video game series.
      */
