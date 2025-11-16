735677b25b6586f9f7180ceee431c3004a73d401
SanaTK1 <sana.hasheminia@gmail.com>
2025-11-16 12:58:33 -0500

    Completed Pokemon Class

--- src/main/java/entity/Pokemon.java
+++ src/main/java/entity/Pokemon.java
@@ -1,7 +1,5 @@
 package entity;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /* By the lab of week eleven, the Pokemon class should be entirely complete so
@@ -16,51 +14,28 @@ public class Pokemon {
     /** Entity representing Pokemon as seen in the Pokemon video game series.
      */
 	private final String name;
-    private HashMap<String, Integer> stats;
+    private BaseLevelStats baseStats;
     private final List<String> types;
     private Move[] moves;
 
-    /** Constructor for Pokemon
-     * @param name the species name of the Pokemon.
-     * @param hp integer representing hit points of the Pokemon
-     * @param attack integer influencing power of physical attacks
-     * @param defense integer influencing defense against physical attacks
-     * @param specialAttack integer influencing power of special attacks
-     * @param specialDefense integer influencing power defense against special attacks
-     * @param speed integer used to determine turn order during battle
-     * @param types string(s) representing Pokemon's type(s). Influences defense against certain attacks
-     * @param moves list of type <Attack> representing the current move this Pokemon can use.
-     *              Max number of possible moves is set four.
-     */
-    public Pokemon(String name,
-                   int hp, int attack, int defense,
-                   int specialAttack, int specialDefense, int speed,
-                   List<String> types, Move[] moves) {
+    public Pokemon(String name, BaseLevelStats stats,
+                   List<String> types) {
         this.name = name;
-        this.types = new ArrayList<String>();
+        this.types = types;
         this.moves = new Move[4];
-        this.stats = new HashMap<String, Integer>(6);
-
-        stats.put("hp", hp); stats.put("attack", attack);
-        stats.put("defense", defense); stats.put("specialAttack", specialAttack);
-        stats.put("specialDefense", specialDefense); stats.put("speed", speed);
+        this.baseStats = stats;
     }
 
-    public boolean isAlive() {
-        return stats.get("hp") > 0;
+    public BattleStats createBattleStats() {
+        return new BattleStats(baseStats);
     }
-
-    //Requires implementation of move and potential clarifying what moves do.
-    //public boolean UseMove(Attack move){}
-
-    //public boolean CanUseMove(Attack move){}
 
     public String getName() {
         return name;
     }
 
-    public HashMap<String, Integer> getStats() {
-        return stats;
+    public BaseLevelStats getBaseStats() {
+        return baseStats;
     }
 
     public List<String> getTypes() {
