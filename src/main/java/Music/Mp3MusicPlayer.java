4169feda4133e820fee9e874765e5a7c35e6b05b
chuhengzheng <chuheng.zheng@mail.utoronto.ca>
2025-12-01 10:58:38 -0500

    change music not just play once

--- src/main/java/Music/Mp3MusicPlayer.java
+++ src/main/java/Music/Mp3MusicPlayer.java
@@ -8,32 +8,49 @@ public class Mp3MusicPlayer implements MusicPlayer {
     private Player player;
     private Thread playThread;
     private final String filePath;
+    private volatile boolean keepPlaying = false;  // controls continuous play
 
     public Mp3MusicPlayer(String filePath) {
         this.filePath = filePath;
     }
 
     @Override
-    public void playMusic() {
-        stopMusic();
-        playThread = new Thread(this::playTask);
+    public synchronized void playMusic() {
+        // If already playing, do nothing
+        if (keepPlaying) {
+            return;
+        }
+
+        keepPlaying = true;
+
+        playThread = new Thread(this::playLoop);
         playThread.start();
     }
 
-    private void playTask() {
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            player = new Player(fis);
-            player.play();
-        } catch (Exception e) {
-            e.printStackTrace();
+    // This keeps looping the track until stopMusic() is called
+    private void playLoop() {
+        while (keepPlaying) {
+            try (FileInputStream fis = new FileInputStream(filePath)) {
+                player = new Player(fis);
+                player.play();  // plays once; when done, loop repeats if keepPlaying is still true
+            } catch (Exception e) {
+                e.printStackTrace();
+                break; // break on error so we don't spin forever
+            }
         }
     }
 
     @Override
-    public void stopMusic() {
+    public synchronized void stopMusic() {
+        keepPlaying = false;     // tell the loop to stop
+
         try {
-            if (player != null) player.close();
-            if (playThread != null && playThread.isAlive()) playThread.interrupt();
+            if (player != null) {
+                player.close();
+            }
+            if (playThread != null && playThread.isAlive()) {
+                playThread.interrupt();
+            }
         } catch (Exception ignored) {}
     }
 }
--- src/main/java/Music/MusicPlayer.java
+++ src/main/java/Music/MusicPlayer.java
@@ -1,7 +1,7 @@
 package Music;
 public interface MusicPlayer {
 
-    // Start (or restart) playing the MP3
+    // Start playing the MP3
     void playMusic();
 
     // Stop playing the MP3
